a3f30d686f718960bb10c4a19833dd7d679ad5a0
Viberiouss <72319842+Viberiouss@users.noreply.github.com>
2023-04-09 13:54:57 -0500

    Add files via upload

---
+++ sites/scraper.js
@@ -0,0 +1,32 @@
+var x = document.querySelectorAll("a");
+var myarray = []
+for (var i = 0; i < x.length; i++) {
+    var nametext = x[i].textContent;
+    let cleantext = nametext.replace(/\s+/g, ' ').replace(".zip", ' ').trim();
+    var cleanlink = x[i].href;
+    cleantext.replace(".zip", "")
+    if (cleanlink.includes(".zip")) {
+        myarray.push([cleantext, cleanlink]);
+    }
+};
+function make_table() {
+    var table = '<table><tbody>';
+    for (var i = 0; i < myarray.length; i++) {
+        table += '<tr><td>' + `<button class='text' onclick="window.location.assign('${myarray[i][1]}')" target="_parent" rel="noopener noreferrer">${myarray[i][0]}</button>` + '</td></tr>';
+    };
+
+    var w = window.open("");
+    w.document.write(table);
+    w.document.body.style.backgroundColor = "#141414"
+    const text = w.document.getElementsByClassName('text')
+    for (let i = 0; i < text.length; i++) {
+        text[i].style.backgroundColor = "transparent";
+        text[i].style.fontFamily = "Courier New";
+        text[i].style.textDecoration = "none";
+        text[i].style.cursor = "pointer";
+        text[i].style.outline = "none";
+        text[i].style.border = "none";
+        text[i].style.display = "list-item"
+    }
+}
+make_table()
